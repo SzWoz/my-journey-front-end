0fb65b0e45ae839ae302554589b2aa7a668f5133
Szymon Woźny <szwoz2001@gmail.com>
2024-05-01 14:57:19 +0200

    basic configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({ include: '**/*.svg' }), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@api': '/src/api',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@data': '/src/data',
+      '@helpers': '/src/helpers',
+      '@types': '/src/types',
+      '@views': '/src/views'
+    }
+  },
+  optimizeDeps: {
+    exclude: ['mui']
+  }
 })
